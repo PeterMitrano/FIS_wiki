31df3af6d510bd221d09bf170c747476158e5c24
peter mitrano <mitranopeter@gmail.com>
2015-09-27 03:11:53 -0400

    moved to subfolder

--- LocalSettings.php
+++ LocalSettings.php
@@ -25,7 +25,7 @@ $wgSitename = "FIS_beta";
 ## For more information on customizing the URLs
 ## (like /w/index.php/Page_title to /wiki/Page_title) please see:
 ## https://www.mediawiki.org/wiki/Manual:Short_URL
-$wgScriptPath = "";
+$wgScriptPath = "/FIS_wiki";
 $wgScriptExtension = ".php";
 
 ## The protocol and server name to use in fully-qualified URLs
